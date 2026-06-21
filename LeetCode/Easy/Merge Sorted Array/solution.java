600dd9caf172eb9ea114cd23594977132d95fbe6
Abhilash Mohapatra <abhilashmohapatra948@gmail.com>
2026-06-21 19:12:04 +0530

    Update solution.java

--- LeetCode/Easy/Merge Sorted Array/solution.java
+++ LeetCode/Easy/Merge Sorted Array/solution.java
@@ -1,11 +1,27 @@
-import java.util.Arrays;
 class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int right=0;
-        for(int left=m;left<m+n;left++){
-            nums1[left]=nums2[right];
-            right++;
+    public void merge(int[] nums1, int m, int[] nums2, int n) 
+    {
+       int r1=m-1;
+       int r2=n-1;
+       int w=m+n-1;
+        while(r1>=0 && r2>=0)
+        {
+            if(nums1[r1]>nums2[r2])
+            {
+                nums1[w]=nums1[r1];
+                r1--;
+            }
+            else
+            {
+                nums1[w]=nums2[r2];
+                r2--;
+            }
+            w--;
+       }
+        while(r2>=0){
+            nums1[w]=nums2[r2];
+            r2--;
+            w--;
         }
-        Arrays.sort(nums1);
-    }
-}
+  }
+} 
