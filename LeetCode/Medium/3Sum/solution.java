8c1be70f993e260a8fa4e99e2199a39628abb28d
Abhilash Mohapatra <abhilashmohapatra948@gmail.com>
2026-06-28 23:00:02 +0530

    Update solution.java

--- LeetCode/Medium/3Sum/solution.java
+++ LeetCode/Medium/3Sum/solution.java
@@ -1,38 +1,37 @@
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-       List<List<Integer>> li = new ArrayList<>();
-       Arrays.sort(nums);
-       int n=nums.length;
-      for (int i=0;i<n-2;i++){
-        if (i>0 && nums[i]==nums[i-1]){
-            continue;
+        Arrays.sort(nums);
+        int n=nums.length;
+        List<List<Integer>> list=new ArrayList<>();
+        for(int i=0;i<n-2;i++){
+           int target=-1*nums[i];
+           int l=i+1;
+           int r=n-1;
+           if(i>0 && nums[i]==nums[i-1])
+             continue;
+           while(l<r){
+             int sum=nums[l]+nums[r];
+             if(sum==target){
+                list.add(Arrays.asList(nums[i],nums[l],nums[r]));
+                l++;
+                r--;
+                while(l<r && nums[l]==nums[l-1]){
+                    l++;
+                }
+                while(l<r  && nums[r]==nums[r+1]){
+                    r--;
+                }
+
+             }
+             else if(sum<target){
+                l++;
+             }
+             else{
+                r--;
+             }
+           }
         }
-          int sum=-1*nums[i];
-          int left=i+1;
-          int right=n-1;
-          while(left<right){
-            int s=nums[left]+nums[right];
-            if(s==sum){
-                li.add(Arrays.asList(nums[i],nums[left],nums[right]));
-                left++;
-                right--;
-                while(left < n && nums[left]==nums[left-1]){
-                    left++;
-                }
-                while(right>= 0 && nums[right]==nums[right+1]){
-                    right--;
-                }
-            }
-            else if (s<sum){
-                left++;
-            }
-            else{
-                right--;
-            }
-          }
-      }
-      return li;
+        return list;
     }
 }
