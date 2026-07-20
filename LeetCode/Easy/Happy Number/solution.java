e103d0310d2cfd8cd0e46c866b31ecf6efc6dcc4
Abhilash Mohapatra <abhilashmohapatra948@gmail.com>
2026-07-20 23:14:40 +0530

    [Syncode] LeetCode/Easy/Happy Number

---
+++ LeetCode/Easy/Happy Number/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int fun(int n){
+        int sum=0;
+        while(n>0){
+            int ld=n%10;
+            sum+=ld*ld;
+            n=n/10;
+        }
+        return sum;
+    }
+    public boolean isHappy(int n) {
+        int slow=n;
+        int fast=n;
+        while(fast!=1){
+            slow=fun(slow);
+            fast=fun(fast);
+            fast=fun(fast);
+            if(slow==fast && slow!=1){
+                return false;
+            }
+        }
+        return true;
+    }
+}
