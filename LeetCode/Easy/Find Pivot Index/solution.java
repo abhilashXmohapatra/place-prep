77a8ebf07ad8f30ace6abcfc03486defac8aaef8
Abhilash Mohapatra <abhilashmohapatra948@gmail.com>
2026-08-17 11:01:31 +0530

    Update solution.java

--- LeetCode/Easy/Find Pivot Index/solution.java
+++ LeetCode/Easy/Find Pivot Index/solution.java
@@ -1,18 +1,15 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-      int left=0;
-      int right=0;
-      int sum=0;
+     int sum=0;
       for(int i:nums){
          sum+=i;
       }
-      if(sum-nums[0]==0)
-      return 0;
-      for(int i=1;i<nums.length;i++){
-        left+=nums[i-1];
-        right=sum-nums[i]-left;
+      int left=0;
+      for(int i=0;i<nums.length;i++){
+        int right=sum-nums[i]-left;
         if(left==right)
         return i;
+        left+=nums[i];
       } 
       return -1;
     }
